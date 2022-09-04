ba4990a79c3fdfd1938402f81689e87bc51fa09b
akbarjon2000 <1543431a@gmail.com>
2022-09-04 22:40:33 +0900

    created auth endpoint

---
+++ vidly/routes/auth.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/users");
+const _ = require("lodash")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+
+
+mongoose.connect("mongodb://localhost/vidly")
+    .then(() => console.log("Conected To Users Database..."))
+    .catch(err => console.log(err))
+
+
+router.post("/", async (req, res) => {
+    // const { error } = validate(req.body);
+    // if (error) return res.status(400).send()
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("invalid email or password")
+
+    const isValid = await bcrypt.compare(req.body.password, user.password)
+    if (!isValid) return res.status(400).send("invalid email or password")
+
+    res.send("authenticated...")
+
+
+})
+
+module.exports = router
