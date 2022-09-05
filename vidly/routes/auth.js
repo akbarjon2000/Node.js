ecddd31630ba545e349be09f8af33e00222c02d9
akbarjon2000 <1543431a@gmail.com>
2022-09-05 10:09:39 +0900

    sent token to the header

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const config = require("config")
 const express = require("express");
 const router = express.Router();
 const { User } = require("../models/users");
@@ -22,7 +23,7 @@ router.post("/", async (req, res) => {
     const isValid = await bcrypt.compare(req.body.password, user.password)
     if (!isValid) return res.status(400).send("invalid email or password")
 
-    const token = jwt.sign({ _id: user._id }, "jsonPrivateKey")
+    const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"))
     res.send(token)
 
 
